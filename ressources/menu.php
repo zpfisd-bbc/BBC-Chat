d129734b3aac4cb5249995f369f4b8d579e4dc5c
Dominic Pfister <dominic.pfister@bbcag.ch>
2015-02-18 14:47:30 +0100

    V0.2.1
    - small bug fixes
    - menu.php added
    - folder sites added

---
+++ ressources/menu.php
@@ -0,0 +1,44 @@
+<?php
+?>
+<!-- Navigation -->
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse"
+				data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span> <span
+					class="icon-bar"></span> <span class="icon-bar"></span> <span
+					class="icon-bar"></span>
+			</button>
+			<!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+			<a class="navbar-brand" href="BBC-Chat|index.php"><img
+				src="img/logo.png" height="40" width="40">BBC Chat</a>
+		</div>
+
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse"
+			id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+
+				<li><a href="../../index.php"><span class="glyphicon glyphicon-globe"></span>
+						Chat</a></li>
+
+				<li><a href="#"> <span class="glyphicon glyphicon-log-in"></span>
+						Login
+				</a></li>
+
+				<li><a href="../sites/register.php"> <span
+						class="glyphicon glyphicon-pencil"></span> Registrieren
+				</a></li>
+
+			</ul>
+		</div>
+
+
+
+		<!-- /.navbar-collapse -->
+	</div>
+	<!-- /.container -->
+</nav>
